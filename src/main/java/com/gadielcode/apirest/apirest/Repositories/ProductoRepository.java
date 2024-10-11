e24e0b5a09e1179addb3bbc1fe496288d446670f
Gadiel <gadiel.salazar44@uabc.edu.mx>
2024-10-10 21:01:25 -0700

    Hicimos una version configurada del proyecyo apirest CRUD par adesplegar en railway

---
+++ src/main/java/com/gadielcode/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,8 @@
+package com.gadielcode.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.gadielcode.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+}
